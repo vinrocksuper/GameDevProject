96ebb270034fec0bec1eb0884960497650158cfc
Vincent <vli8558@bths.edu>
2019-02-21 13:27:38 -0500

    Csvreader set up. Needs Controller in place to work. _        _  \_(ツ)_/

---
+++ src/sample/Backend.java
@@ -0,0 +1,52 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import sample.Question;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Backend {
+
+
+    public static List<Question> fromCSV() {
+
+
+        List<Question> z = new ArrayList<>();
+
+        /**
+         * Note to self:
+         *
+         * Change scores.csv to w/e the csv is going to be named.
+         */
+        Path pathToFile = Paths.get("src/scores.csv");
+
+        try (BufferedReader br = Files.newBufferedReader(pathToFile, StandardCharsets.UTF_8)) {
+            String line = br.readLine();
+            if (line == null) {
+                return z;
+            }
+            while (line != null) {
+                String[] attributes = line.split(",");
+
+                Question temp = Question.createQuestion(attributes);
+
+                z.add(temp);
+                line = br.readLine();
+
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return z;
+    }
+}
--- src/sample/Question.java
+++ src/sample/Question.java
@@ -1,7 +1,7 @@
 package sample;
 
 public class Question {
-    private String prompt;
+    private String scenario;
     private String yes;
     private String no;
     private int yPollution;
@@ -13,9 +13,9 @@ public class Question {
     private int nSupport;
     private int nBiodiversity;
 
-    public Question(String p,String y, String n, int yP, int yM, int yS, int yB, int nP, int nM, int nS, int nB)
+    public Question(String s,String y, String n, int yP, int yM, int yS, int yB, int nP, int nM, int nS, int nB)
     {
-        prompt = p;
+        scenario = s;
         yes =y;
         no = n;
         yPollution = yP;
@@ -28,8 +28,8 @@ public class Question {
         nBiodiversity = nB;
     }
 
-    public String getPrompt() {
-        return prompt;
+    public String getScenario() {
+        return scenario;
     }
 
     public String getYes() {
@@ -70,5 +70,20 @@ public class Question {
 
     public int getnBiodiversity() {
         return nBiodiversity;
+    }
+    public static Question createQuestion(String[] info)
+    {
+        String sc = info[0];
+        String yP = info[1];
+        String nP = info[2];
+        int yPol = Integer.parseInt(info[3]);
+        int yMon = Integer.parseInt(info[4]);
+        int ySup = Integer.parseInt(info[5]);
+        int yBio = Integer.parseInt(info[6]);
+        int nPol = Integer.parseInt(info[7]);
+        int nMon = Integer.parseInt(info[8]);
+        int nSup = Integer.parseInt(info[9]);
+        int nBio = Integer.parseInt(info[10]);
+        return new Question(sc,yP,nP,yPol,yMon,ySup,yBio,nPol,nMon,nSup,nBio);
     }
 }
