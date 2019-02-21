34debae9f4f0a8524d17a5750c3b3a670ccbf0c8
ducksmen <jhoang6606@bths.edu>
2019-02-21 14:27:42 -0500

    controller ids and stuff

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,19 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.Initializable;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Controller
+{
+    public Label question;
+    public Label optionOne;
+    public Label optionTwo;
+
+    public void startGame(ActionEvent event)
+    {
+
+    }
 
 }
